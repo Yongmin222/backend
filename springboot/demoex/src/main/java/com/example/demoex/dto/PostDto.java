9704810323183f514873b5100c5dca3c0ce27163
Yongmin <Yongmin222>
2024-12-26 17:40:06 +0900

    Feat: SB JPA PostController 구성, PostService DI 적용, PostDto 작성

---
+++ springboot/demoex/src/main/java/com/example/demoex/dto/PostDto.java
@@ -0,0 +1,39 @@
+package com.example.demoex.dto;
+
+import com.example.demoex.entities.Post;
+import com.example.demoex.entities.Review;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 데이터 교환용, Post 엔티티의 데이터를 가져와서 컨트로럴에서 사용됨
+ * - 테이블과 직접 연결성 X -> 보안에 우수
+ * - 구성 :
+ *  - 1. Post(타겟 엔티티)의 필드를 모두 복사하여 필드(맴버변수)만 표시함
+ *  - 2. 롬복 처리
+ *      @Setter
+ *      @Getter
+ *      @ToString
+ *      @Builder
+ *  - 3. 엔티티 변환 처리 가능함 메소드 구성 -> toEntity()
+ */
+@Setter
+@Getter
+@ToString
+@Builder
+public class PostDto {
+    private int id;
+    private String subject;
+    private String content;
+    private LocalDateTime createDate;
+    private List<Review> reviews;
+
+    public Post toEntity() {
+        return
+    }
+}
