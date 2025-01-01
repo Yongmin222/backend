65a582ed97118f143930fee301d8c6b9685bfae0
Yongmin <Yongmin222>
2025-01-01 17:01:41 +0900

    Feat: SB 게시판 리퓨 관련 컨트롤러, 서비스 구성중, DTO 구성

---
+++ springboot/demoex/src/main/java/com/example/demoex/dto/ReviewDto.java
@@ -0,0 +1,30 @@
+package com.example.demoex.dto;
+
+import com.example.demoex.entities.Post;
+import com.example.demoex.entities.Review;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@ToString
+@Builder
+public class ReviewDto {
+    // 실습 :  진행 내부 구성, Review 엔티티 대상으로 수행
+    private int id;
+    private String content;
+    private LocalDateTime createDate;
+    private Post post;
+    public Review toEntity() {
+        return Review.builder()
+                .id(id)
+                .content(content)
+                .createDate(createDate)
+                .post(post)
+                .build();
+    }
+}
