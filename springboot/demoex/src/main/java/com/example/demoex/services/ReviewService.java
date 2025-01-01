84e26d89fd4fa62d3df010ae3879510eedd3b82d
Yongmin <Yongmin222>
2025-01-01 15:44:29 +0900

    Feat: SB 게시판 리뷰/댓글 기능(등록, 수정, 삭제)를 위한 컨트롤러, 라우트,서비스, 레퍼지토리 구성

---
+++ springboot/demoex/src/main/java/com/example/demoex/services/ReviewService.java
@@ -0,0 +1,11 @@
+package com.example.demoex.services;
+
+import lombok.RequiredArgsConstructor;
+import org.example.demoex.repositories.ReviewRepository;
+import org.springframework.stereotype.Service;
+@RequiredArgsConstructor
+@Service
+public class ReviewService {
+    // DI, 생성자 방식 구현
+    private final ReviewRepository reviewRepository;
+}
